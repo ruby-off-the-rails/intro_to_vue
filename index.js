b2a3ff5c87fd7b47dd29d2c0b165e6add150f007
brian <riceb53@gmail.com>
2019-12-09 18:35:11 -0600

    add v-on:click and v-model

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ var app = new Vue({
   data: {
     message: "Hello Vue!",
     date: "Monday"
+  },
+  methods: {
+    changeMessage: function() {
+      console.log('changing the message....')
+      this.message = "something else";
+    }
   }
 });
